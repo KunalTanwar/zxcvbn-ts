daae2db79039fb5cbfdfe6a53c90b98fcd677275
Kunal Tanwar <iamkunaltanwar@gmail.com>
2026-03-24 00:35:39 +0530

    perf: reduced unpacked size from `6.12 MB` to `1.1 MB`

--- src/frequency_lists.ts
+++ src/frequency_lists.ts
@@ -2,8 +2,9 @@
 // across CJS, ESM, and types build targets.
 // The JSON file is copied alongside each compiled output by the build scripts.
 import type { FrequencyLists } from "./types"
-import data from "../data/frequency_lists.json"
 
-const frequencyLists: FrequencyLists = data as FrequencyLists
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const frequencyLists: FrequencyLists =
+    require("../data/frequency_lists.json") as FrequencyLists
 
 export default frequencyLists
